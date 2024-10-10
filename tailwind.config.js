97fcdde9cab0a8e6bb70f7e17378344fe2c6ef47
Matthew Cochran <131500068+mcochranca@users.noreply.github.com>
2024-10-10 12:31:32 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }, // Example fade in animation 
       colors: {
         primary: '#2563eb', // Custom primary color
         secondary: '#1e293b', // Custom secondary color
